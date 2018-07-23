af2b2940734ebef1b684bc5c6e75fc6e094a99d1
christian.phu <christian.phu@fr.clara.net>
2018-07-23 17:05:15 +0200

    TER-306-Fix_mysql_configuration

--- mysql.tf
+++ mysql.tf
@@ -1,5 +1,5 @@
 resource "azurerm_mysql_server" "mysql_server" {
-  name                         = "mysql-${var.environment}-${var.azure_region_short}-${var.client_name}-${var.stack}"
+  name                         = "mysql-${var.environment}-${var.azure_region_short}-${var.client_name}-${var.stack}-${var.mysql_name}"
   location                     = "${var.azure_region}"
   resource_group_name          = "${var.resource_group_name}"
   sku                          = ["${var.server_sku}"]
@@ -21,8 +21,8 @@ resource "azurerm_mysql_database" "mysql_db" {
 
 resource "azurerm_mysql_configuration" "config" {
   count               = "${length(var.mysql_options)}"
-  name                = "${lookup(element(var.mysql_options, count.index), "name")}"
+  name                = "${lookup(var.mysql_options[count.index], "name")}"
   resource_group_name = "${var.resource_group_name}"
   server_name         = "${azurerm_mysql_server.mysql_server.name}"
-  value               = "${lookup(element(var.mysql_options, count.index), "value")}"
+  value               = "${lookup(var.mysql_options[count.index], "value")}"
 }
