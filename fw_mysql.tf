cda40a7faec743dec3dac575d6d79daf698388f2
christian.phu <christian.phu@fr.clara.net>
2018-07-23 09:58:27 +0200

    TER-306-Change_firewall_rule

--- fw_mysql.tf
+++ fw_mysql.tf
@@ -1,5 +1,5 @@
 resource "azurerm_mysql_firewall_rule" "mysql_rule" {
-  name                = "mysql-rule-${count.index}"
+  name                = "mysql-rule-${replace(cidrhost(element(var.admin_cidrs, count.index), 0),".","-")}"
   count               = "${length(var.admin_cidrs)}"
   resource_group_name = "${var.resource_group_name}"
   server_name         = "${azurerm_mysql_server.mysql_server.name}"
@@ -9,7 +9,7 @@ resource "azurerm_mysql_firewall_rule" "mysql_rule" {
 
 resource "azurerm_mysql_firewall_rule" "webapp_rule" {
   count               = "${var.webapp_enabled == "true" ?  var.length_webapp_ip : 0 }"
-  name                = "mysql-rule-webappi-${count.index}"
+  name                = "mysql-rule-webapp-${replace(element(var.mysql_ip, count.index),".","-")}"
   resource_group_name = "${var.resource_group_name}"
   server_name         = "${azurerm_mysql_server.mysql_server.name}"
   start_ip_address    = "${element(var.mysql_ip, count.index)}"
