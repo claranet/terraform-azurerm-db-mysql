54b8d9684d933001892525d5e3c94a80afa6cc33
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2019-11-27 15:21:30 +0100

    AZ-149: Improve VNet rules use and fix output

--- r-vnetrules.tf
+++ r-vnetrules.tf
@@ -1,7 +1,12 @@
 resource "azurerm_mysql_virtual_network_rule" "vnet_rules" {
-  count               = length(var.vnet_rules)
-  name                = lookup(var.vnet_rules[count.index], "name", count.index)
+  count = length(var.allowed_subnets)
+
+  name = format(
+    "%s-%s",
+    element(split("/", var.allowed_subnets[count.index]), 8), # VNet name
+    element(split("/", var.allowed_subnets[count.index]), 10) # Subnet name
+  )
   resource_group_name = var.resource_group_name
   server_name         = azurerm_mysql_server.mysql_server.name
-  subnet_id           = lookup(var.vnet_rules[count.index], "subnet_id")
+  subnet_id           = var.allowed_subnets[count.index]
 }
