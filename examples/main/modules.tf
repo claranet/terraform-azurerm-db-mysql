1a14400be42994c19becd87b6462a83c04933c31
Rémi Beaufils <remi.beaufils@fr.clara.net>
2021-09-20 12:28:30 +0200

    AZ-572: Revamp examples + improve CI

---
+++ examples/main/modules.tf
@@ -0,0 +1,74 @@
+module "azure_region" {
+  source  = "claranet/regions/azurerm"
+  version = "x.x.x"
+
+  azure_region = var.azure_region
+}
+
+module "rg" {
+  source  = "claranet/rg/azurerm"
+  version = "x.x.x"
+
+  location    = module.azure_region.location
+  client_name = var.client_name
+  environment = var.environment
+  stack       = var.stack
+}
+
+module "logs" {
+  source  = "claranet/run-common/azurerm//modules/logs"
+  version = "x.x.x"
+
+  client_name         = var.client_name
+  environment         = var.environment
+  stack               = var.stack
+  location            = module.azure_region.location
+  location_short      = module.azure_region.location_short
+  resource_group_name = module.rg.resource_group_name
+}
+
+module "mysql" {
+  source  = "claranet/db-mysql/azurerm"
+  version = "x.x.x"
+
+  client_name    = var.client_name
+  environment    = var.environment
+  location       = module.azure_region.location
+  location_short = module.azure_region.location_short
+  stack          = var.stack
+
+  resource_group_name = module.rg.resource_group_name
+
+  tier     = "GeneralPurpose"
+  capacity = 4
+
+  allowed_cidrs = ["10.0.0.0/24", "12.34.56.78/32"]
+
+  storage_mb                   = 5120
+  backup_retention_days        = 10
+  geo_redundant_backup_enabled = true
+  auto_grow_enabled            = false
+
+  administrator_login    = var.administrator_login
+  administrator_password = var.administrator_password
+  databases_names        = ["my_database"]
+
+  force_ssl     = true
+  mysql_options = [{ name = "interactive_timeout", value = "600" }, { name = "wait_timeout", value = "260" }]
+  mysql_version = "5.7"
+  databases_charset = {
+    "my_database" = "utf8"
+  }
+  databases_collation = {
+    "my_database" = "utf8_general_ci"
+  }
+
+  logs_destinations_ids = [
+    module.logs.logs_storage_account_id,
+    module.logs.log_analytics_workspace_id
+  ]
+
+  extra_tags = {
+    foo = "bar"
+  }
+}
